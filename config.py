64bd2d4fda61d92cf7f681c4e5258bbf97ba1f8e
malakaminwork-dot <malakaminwork@gmail.com>
2026-02-09 11:55:04 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# توكن البوت
+BOT_TOKEN = os.getenv('BOT_TOKEN', 'ضع_التوكن_هنا')
+
+# معرف المطور (اختياري)
+DEVELOPER_ID = os.getenv('DEVELOPER_ID', '')
+
+# أنواع الأسئلة
+QUESTION_TYPES = {
+    'true_false': 'صح أو خطأ',
+    'multiple_choice': 'اختيار من متعدد',
+    'short_answer': 'إجابة قصيرة'
+}
